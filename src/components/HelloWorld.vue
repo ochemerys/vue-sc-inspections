90ea0432fc8a96395fe7433f0e92ea1f374a3ec1
Oleks Chemerys <oleks@quick.cz>
2020-11-11 18:06:30 -0700

    staging branch is updated

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <p>Staging branch is added</p>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
